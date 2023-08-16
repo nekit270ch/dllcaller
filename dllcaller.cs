0222bf77c58d0e17c1f07bec05268065cf3e4322
nekit270 <126622890+nekit270ch@users.noreply.github.com>
2023-08-17 01:15:20 +0300

    Add files via upload

--- dllcaller.cs
+++ dllcaller.cs
@@ -46,6 +46,30 @@ namespace DllCallerLib{
             return delegateType;
         }
 
+        private static Type CreateDelegate(string returnType, string[] argumentTypes){
+            Type delegateReturnType = Type.GetType(returnType);
+
+            Type[] delegateArgumentTypes = new Type[argumentTypes.Length];
+            for(int i = 0; i < argumentTypes.Length; i++){
+                delegateArgumentTypes[i] = Type.GetType(argumentTypes[i]);
+            }
+
+            AssemblyName assemblyName = new AssemblyName("DynamicAssembly");
+            AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("DynamicModule");
+
+            TypeBuilder typeBuilder = moduleBuilder.DefineType("DynamicDelegate", TypeAttributes.Public | TypeAttributes.Sealed | TypeAttributes.Class, typeof(MulticastDelegate));
+
+            ConstructorBuilder constructorBuilder = typeBuilder.DefineConstructor(MethodAttributes.RTSpecialName | MethodAttributes.HideBySig | MethodAttributes.Public, CallingConventions.Standard, new Type[] { typeof(object), typeof(IntPtr) });
+            constructorBuilder.SetImplementationFlags(MethodImplAttributes.Runtime | MethodImplAttributes.Managed);
+
+            MethodBuilder methodBuilder = typeBuilder.DefineMethod("Invoke", MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.Virtual, delegateReturnType, delegateArgumentTypes);
+            methodBuilder.SetImplementationFlags(MethodImplAttributes.Runtime | MethodImplAttributes.Managed);
+
+            Type delegateType = typeBuilder.CreateType();
+            return delegateType;
+        }
+
         public static T CallFunction<T>(string dllName, string funcName, List<Argument> args){
             return CallFunction<T>(dllName, funcName, args.ToArray());
         }
@@ -65,6 +89,27 @@ namespace DllCallerLib{
             Type del = CreateDelegate<T>(argTypes.ToArray());
 
             return (T)(Marshal.GetDelegateForFunctionPointer(fn, del).DynamicInvoke(argValues.ToArray()));
+        }
+
+        public static object CallFunction(string dllName, string funcName, string type, List<Argument> args){
+            return CallFunction(dllName, funcName, type, args.ToArray());
+        }
+
+        public static object CallFunction(string dllName, string funcName, string type, Argument[] args){
+            IntPtr mod = LoadLibrary(dllName);
+            IntPtr fn = GetProcAddress(mod, funcName);
+
+            List<string> argTypes = new List<string>();
+            List<object> argValues = new List<object>();
+
+            foreach(Argument arg in args){
+                argTypes.Add(arg.Type);
+                argValues.Add(arg.Value);
+            }
+
+            Type del = CreateDelegate(type, argTypes.ToArray());
+
+            return Marshal.GetDelegateForFunctionPointer(fn, del).DynamicInvoke(argValues.ToArray());
         }
     }
 }
